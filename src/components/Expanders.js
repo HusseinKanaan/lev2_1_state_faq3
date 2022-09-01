bd8f47af1406f05223ffc92a0d31e4bb0ae53ca3
HusseinKanaan <107847813+HusseinKanaan@users.noreply.github.com>
2022-09-02 01:18:54 +0200

    first commit

---
+++ src/components/Expanders.js
@@ -0,0 +1,20 @@
+import './Expanders.css'
+import FaqItem from './FaqItem'
+import faq from './data'
+
+const Expand = () => {
+    return (
+        <>
+            {faq.map((elt) =>
+                <FaqItem
+                    question={elt.question}
+                    answer={elt.answer}
+                    answerLong={elt.answerLong}
+                />
+            )
+            }
+        </>
+    )
+
+}
+export default Expand
